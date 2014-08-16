8ae150661315a034671c126849cf60319ff1d353
Fantasy Shao <forever.fantasy27@gmail.com>
2014-08-16 18:01:32 +0800

    v0.1.0 Basic translate function.

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ var url = config.src;
 
 module.exports = function(param) {
 
-  request(url + param, function(err, res, body) {
+  request(url + encodeURIComponent(param), function(err, res, body) {
     var data;
 
     if (!err && res.statusCode === 200) {
       data = JSON.parse(body);
-
-      console.log(data.translation[0]);
+      if (data.errorCode == 0) {
+        lib.output(data);
+      } else {
+        console.error('[ERROR]'.red + ' Youdao API request error.');
+      }
     }
   });
 };
