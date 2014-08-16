21ed6b2f5ca19551b98356cf857c50ea9b41a37d
Fantasy Shao <forever.fantasy27@gmail.com>
2014-08-16 23:37:00 +0800

    Fix code style.

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var url = config.src;
 module.exports = function(param) {
   
   if (param.length == 0) {
-        console.log('[Warning]'.yellow + "Nothing to be translated");
-        return;
-      };
+    console.log('[Warning]'.yellow + "Nothing to be translated");
+    return;
+  }
 
   request(url + encodeURIComponent(param), function(err, res, body) {
     var data;
@@ -23,8 +23,8 @@ module.exports = function(param) {
       if (data.errorCode == 0 && data.basic != null) {
         lib.output(data);
       } else if(data.errorCode == 0 && data.basic  == null) {
-        console.log('[Warning]'.yellow + "No translation fit for the word " + ('['+param+']').red);
-      }else{
+        console.log('[Warning]'.yellow + 'No translation fit for the word ' + ('[' + param + ']').red);
+      } else {
         console.error('[ERROR]'.red + ' Youdao API request error.');
       }
     }
