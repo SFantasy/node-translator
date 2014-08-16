6794be346a5124cf46684b4506004c11e11bc81d
Galeno leng <lenghan1991@gmail.com>
2014-08-16 23:23:29 +0800

    test

--- index.js
+++ index.js
@@ -4,20 +4,27 @@
 var request = require('request');
 
 var lib = require('./lib'),
-    config = require('./config');
+config = require('./config');
 
 var url = config.src;
 
 module.exports = function(param) {
+  
+  if (param.length == 0) {
+        console.log('[Warning]'.yellow + "Nothing to be translated");
+        return;
+      };
 
   request(url + encodeURIComponent(param), function(err, res, body) {
     var data;
 
     if (!err && res.statusCode === 200) {
       data = JSON.parse(body);
-      if (data.errorCode == 0) {
+      if (data.errorCode == 0 && data.basic != null) {
         lib.output(data);
-      } else {
+      } else if(data.errorCode == 0 && data.basic  == null) {
+        console.log('[Warning]'.yellow + "No translation fit for the word " + ('['+param+']').red);
+      }else{
         console.error('[ERROR]'.red + ' Youdao API request error.');
       }
     }
